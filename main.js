b331bc0aee96e3c1625f72b6d3eb98575e3f9e47
Boyang Matsapola <boyangbass@gmail.com>
2019-07-19 20:30:14 -0500

    fixed parallax issue

--- main.js
+++ main.js
@@ -1,29 +1,33 @@
 window.onload = function() {
 var arrowDown = document.getElementById('arrowDown');
-function expandNav(){
-    var menuNav= document.getElementById("menu-nav");
-    if(menuNav.style.display === "none"){
-        menuNav.style.display="block";
-        menuNav.style.height= "200px";
+var menuNav= document.getElementById("menu-nav");
+var music = document.querySelector('.music');
+var parallax = document.querySelector('.parallax');
+
+
+    
+    function expandNav(){
+        if(menuNav.style.display === "none"){
+            menuNav.style.display="block";
+            menuNav.style.height= "200px";
+            
+        }else{
+            menuNav.style.display="none";
+            menuNav.style.height= "0px";
+        }
+    }
+
+    arrowDown.addEventListener('click',expandNav);
+    document.addEventListener('scroll',function(){
+        var navBar= document.getElementById('wrapper');
+        if(window.pageYOffset >= 100 ){
+            navBar.style.opacity="0.8";
+            navBar.style.background="lightblue";
+        }else{
+            navBar.style.background="transparent";
+        };
+    });
         
-    }else{
-        menuNav.style.display="none";
-        menuNav.style.height= "0px";
-    }
-}
-arrowDown.addEventListener('click',expandNav);
-window.addEventListener('scroll',function(){
-    var navBar= document.getElementById('wrapper');
-    if(window.pageYOffset >= 50 ){
-        navBar.style.opacity="0.8";
-        navBar.style.background="lightblue";
-    }else{
-        navBar.style.background="transparent";
-    }
-});
-
-
-
-
 
 }
+
